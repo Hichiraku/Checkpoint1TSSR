96c6abae0a8da5428bc59f05660fdfc0c2653e48
Hichiraku <vincent.delciotto@gmail.com>
2023-10-15 18:09:31 +0200

    Script en boucle fonctionnel

--- addUsers.sh
+++ addUsers.sh
@@ -3,7 +3,7 @@
 #####################################
 #       Vincent Del Ciotto          #
 #           Script 0.1              #
-#Création de 3 utilisateur simultané# 
+#Création d'utilisateur simultané   # 
 # 	  le 13/10/2023             #
 #####################################
 
@@ -17,31 +17,21 @@
 #pas de fonction pour l'instant
 
 #############################Main###################################
-#verification argument non vide
-if [ "$1" = "" ] || [ "$2" = "" ] || [ "$3" = "" ]
+if [ -z $1 ]
 then
-	echo "Il manque les noms d'utilisateurs en argument - Fin du script"
+	echo "Il manque les noms d'utilisateurs en argument - Fin du script" 
 	exit 1
-#verification existance utilisateur
 else
-	if cat /etc/passwd | grep "$1" > /dev/null
-	then
-		echo "L'utilisateur $1 existe déjà"
-		exit 1
-	else
-		if cat /etc/passwd  | grep "$2" > /dev/null
+	for user in $*
+	do
+		if cat /etc/passwd | grep $user > /dev/null
 		then 
-			echo " L'utilisateur $2 existe déja"
+			echo "L'utilisateur $user existe déjà"
 			exit 1
 		else
-			if cat /etc/passwd | grep "$3" > /dev/null
-			then 
-				echo " L'utilisateur $3 existe déja"
-				exit 1
-			else
-#création des 3 utilisateur
-		useradd $1 && useradd $2 && useradd $3 && echo "Les utilisateurs $1 $2 $3 ont été crées avec succès"
-			fi
-		fi		
-	fi
+			useradd $user &&  echo "L'utilisateur $user a été crée"
+			exit 0
+		fi
+	done
 fi
+
